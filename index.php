e3a88b40b617dfd0dfb90a10f64325fbe34a7292
Deivid Nn <deividnn@users.noreply.github.com>
2016-01-23 15:01:00 -0200

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        require_once('confi.php');
-         $qur = mysql_query('select * from users');
-         echo $qur.'aki';
-        // put your code here
-        ?>
+       
     </body>
 </html>
